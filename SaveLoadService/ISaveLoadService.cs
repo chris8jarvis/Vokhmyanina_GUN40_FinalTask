9f06b43ebcb40668fa5458cb512923c62cae7438
chris8jarvis <christinaeduardovna8@gmail.com>
2026-01-22 19:55:15 +0300

    first step

---
+++ SaveLoadService/ISaveLoadService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vokhmyanina_GUN40_FinalTask.SaveLoadService
+{
+    public interface ISaveLoadService<T>
+    {
+        void SaveData (T data, string id);
+        T LoadData(string id);
+
+    }
+}
